ea1a126203fdffa981ff6a80efeadcf2ba04e31e
EnVy <envy7321@gmail.com>
2022-08-17 13:45:10 +0300

    save

--- src/js/simpleLightBox.js
+++
@@ -1,9 +0,0 @@
-
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-new SimpleLightbox(".gallery a", {
-    captionSelector: "img",
-    captionsData: "alt",
-    captionPosition: "bottom",
-    captionDelay: 250,
-  });
